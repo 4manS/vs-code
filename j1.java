ece45b4f77516fba22671b82827b54e5031b2f29
4man <a.k.singh0318@gmail.com>
2023-06-17 05:31:06 +0530

    first commit

---
+++ j1.java
@@ -0,0 +1,11 @@
+//to run the java program run command= java j1.java      or      click on run button on right upper corner
+public class j1 {
+    public static void main(String[] args) {
+        System.out.println("sum of 10+20+30 is:");
+        int a=10;
+        int b=20;
+        int c=30;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
